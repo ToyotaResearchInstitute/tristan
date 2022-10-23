c4c92dc58ebe45518d4d15fab73768a9f7ef6a14
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-23 00:46:25 +0000

    Adding tarfile member sanitization to extractall()

--- intent/multiagents/cache_utils.py
+++ intent/multiagents/cache_utils.py
@@ -241,7 +241,26 @@ def download_and_extract(cache_dir, s3_key):
     tar_target = os.path.join(cache_dir, s3_key_filename)
     print(f"extracting cache tar: {tar_target} to ")
     with tarfile.open(tar_target) as tar:
-        tar.extractall(cache_dir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, cache_dir)
 
 
 torch_ver_to_conda_env = {
